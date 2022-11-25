821116a98bb10bf285dc8ab9a7c252e797049bde
Vladislav Pronoza <108750974+longrinn@users.noreply.github.com>
2022-11-25 19:03:53 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 if ! [ -x "$(command -v compose)" ]; then
   echo -e 'Error: docker compose is not installed.\n Installing docker...'
-  sudo -y apt update && sudo apt -y install ca-certificates curl gnupg lsb-relese
+  sudo apt -y update && sudo apt -y install ca-certificates curl gnupg lsb-relese
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
