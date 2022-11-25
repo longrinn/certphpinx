da5dfccf0edf897c88201f7eb5c89e26919096e5
Vladislav Pronoza <108750974+longrinn@users.noreply.github.com>
2022-11-25 19:42:02 +0200

    minor changes

--- init.sh
+++ init.sh
@@ -2,13 +2,12 @@
 
 if ! [ -x "$(command -v compose)" ]; then
   echo -e 'Error: docker compose is not installed.\n Installing docker...'
-  sudo apt -y update && sudo apt -y install ca-certificates curl gnupg lsb-relese
-  sudo mkdir -p /etc/apt/keyrings
+  apt update && sudo apt -y install ca-certificates curl gnupg lsb-relese
+  mkdir -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt update
-  sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  apt update && apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
   exit 1
 fi
 
@@ -17,7 +16,8 @@ if [ "$#" -ne 2 ] || [ $1 != "-d" ]; then
   exit 1
 fi
 
-if [ -f "./nginx/default.conf" ]; then
+if [ -f "./nginx/site_config.conf" ]; then
+    mv ./nginx/site_config.conf ./nginx/$2.conf
     sed -i "s/<-->/$2/g" "./nginx/$2.conf"
     sed -i "s/<-->/$2/g" "docker-compose.yml"
 fi
