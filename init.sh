052f13893d112784b2931e8d7c66a97927285836
Vladislav Pronoza <108750974+longrinn@users.noreply.github.com>
2022-11-25 18:41:59 +0200

    added docker* installation

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v compose)" ]; then
-  echo 'Error: docker compose is not installed.' >&2
+  echo -e 'Error: docker compose is not installed.\n Installing docker...'
+  sudo -y apt update && sudo apt -y install ca-certificates curl gnupg lsb-relese
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update
+  sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
   exit 1
 fi
 
